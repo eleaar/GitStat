83e8ab2eed1bca0edb355f4c2cd879ef80f6978a
Daniel Krzywicki <eleaar@gmail.com>
2014-12-07 21:35:47 +0100

    Initial crude stats view

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,7 +3,7 @@ package controllers
 import java.net.ConnectException
 
 import logic.GitHubService
-import logic.GitHubService.RateExceeded
+import logic.GitHubService._
 import org.joda.time.{DateTime, Seconds}
 import play.api.Play.current
 import play.api.libs.ws.WS
@@ -28,6 +28,21 @@ object Application extends Controller {
       case RateExceeded(time) =>
         val seconds = Seconds.secondsBetween(DateTime.now, new DateTime(time * 1000)).getSeconds
         Forbidden(s"Rate exceeded. Please try again in $seconds s")
+    }
+  }
+
+  def stats(user: String, repo: String) = Action.async {
+    gitHubService.stats(user, repo).map {
+      case commits =>
+        Ok(views.html.stats(s"$user/$repo", commits))
+    } recover {
+      case error: ConnectException =>
+        ServiceUnavailable("Could not connect to Github")
+      case RateExceeded(time) =>
+        val seconds = Seconds.secondsBetween(DateTime.now, new DateTime(time * 1000)).getSeconds
+        Forbidden(s"Rate exceeded. Please try again in $seconds s")
+      case NotFoundException =>
+        NotFound(s"Sorry, couldn't find $user/$repo. Did you spell it right?")
     }
   }
 }
