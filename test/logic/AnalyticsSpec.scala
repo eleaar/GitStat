cd4299dd2001f22e2a659a583a2ee6ab98757525
Daniel Krzywicki <eleaar@gmail.com>
2014-12-17 19:25:00 +0100

    fix deprecated analitycs tests

--- test/logic/AnalyticsSpec.scala
+++ test/logic/AnalyticsSpec.scala
@@ -16,11 +16,15 @@ class AnalyticsSpec extends PlaySpec {
 
     def contributor(id: Long) = Some(Contributor(s"user$id", id))
 
-    def createInfo(contributor: Option[Contributor], count: Int) = Seq.tabulate(count)(i => CommitInfo(s"sha$i", DateTime.now, contributor))
+    val now = DateTime.now.withTimeAtStartOfDay()
+
+    def createInfo(contributor: Option[Contributor], count: Seq[(DateTime, Int)]) = count.flatMap {
+      case (date,count) => Seq.tabulate(count)(i => CommitInfo(s"sha$i", date, contributor))
+    }
 
     "Count the commits per user" in {
       // given
-      val expectedCount = Seq(contributor(1) -> 5, contributor(2) -> 10)
+      val expectedCount = Seq(contributor(1) -> Seq(now -> 5), contributor(2) -> Seq(now -> 10))
       val commitsInfo = expectedCount.flatMap(x => createInfo(x._1, x._2))
 
       // when
@@ -32,7 +36,7 @@ class AnalyticsSpec extends PlaySpec {
 
     "Handle empty commiters" in {
       // given
-      val expectedCount: Seq[(Option[Contributor], Int)] = Seq(contributor(1) -> 5, None -> 5)
+      val expectedCount = Seq(contributor(1) -> Seq(now -> 5), None -> Seq(now -> 5))
       val commitsInfo = expectedCount.flatMap(x => createInfo(x._1, x._2))
 
       // when
@@ -40,63 +44,6 @@ class AnalyticsSpec extends PlaySpec {
 
       // then
       result must contain theSameElementsAs expectedCount
-    }
-
-    "Sort commits in descending order" in {
-      // given
-      val expectedCount = Seq(contributor(1) -> 10, contributor(2) -> 5, contributor(3) -> 1)
-      val commitsInfo = expectedCount.flatMap(x => createInfo(x._1, x._2))
-      val shuffledInput = Random.shuffle(commitsInfo)
-
-      // when
-      val result = Analytics.commitsPerUser(commitsInfo)
-
-      // then
-      result must contain theSameElementsInOrderAs expectedCount
-    }
-
-    "Convert dates to localdates using given zone" in {
-      // given
-      implicit val zone = DateTimeZone.getDefault()
-      val dateTime = new DateTime("2004-12-13T21:39:45")
-      val localDate = new LocalDate(dateTime, zone)
-      val commitsInfo = Seq(CommitInfo("sha", dateTime, None))
-
-      // when
-      val result = Analytics.commitsPerDate(commitsInfo)
-
-      // then
-      result must contain theSameElementsAs Seq(localDate -> Seq(None -> 1))
-    }
-
-    "Group commits within dates" in {
-      // given
-      implicit val zone = DateTimeZone.getDefault()
-      val dateTime = DateTime.now
-      val localDate = new LocalDate(dateTime, zone)
-      val commitsInfo = Seq(CommitInfo("sha", dateTime, None), CommitInfo("sha", dateTime, None))
-
-      // when
-      val result = Analytics.commitsPerDate(commitsInfo)
-
-      // then
-      result must contain theSameElementsAs Seq(localDate -> Seq(None -> 2))
-    }
-
-    "Sort dates in descending order" in {
-      // given
-      implicit val zone = DateTimeZone.getDefault()
-      val dateTime1 = DateTime.now
-      val dateTime2 = dateTime1 + 1.month
-      val localDate1 = new LocalDate
-      val localDate2 = new LocalDate(dateTime2, zone)
-      val commitsInfo = Seq(CommitInfo("sha", dateTime1, None), CommitInfo("sha", dateTime2, None))
-
-      // when
-      val result = Analytics.commitsPerDate(commitsInfo)
-
-      // then
-      result must contain theSameElementsInOrderAs Seq(localDate2 -> Seq(None -> 1), localDate1 -> Seq(None -> 1))
     }
   }
 }
